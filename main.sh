a6d3b37a8d274595e5be85b9a3df10f1ec74b18f
Prajwal Koirala <16564273+Prajwal-Koirala@users.noreply.github.com>
2021-07-09 13:34:14 -0400

    Update main.sh

--- main.sh
+++ main.sh
@@ -1,1 +1,27 @@
 #!/bin/bash
+# https://github.com/complexorganizations/github-codespaces-system
+
+# Require script to be run as root
+function super-user-check() {
+  if [ "${EUID}" -ne 0 ]; then
+    echo "You need to run this script as super user."
+    exit
+  fi
+}
+
+# Check for root
+super-user-check
+
+# Detect Operating System
+function dist-check() {
+  if [ -f /etc/os-release ]; then
+    # shellcheck disable=SC1091
+    source /etc/os-release
+    DISTRO=${ID}
+    DISTRO_VERSION=${VERSION_ID}
+  fi
+}
+
+# Check Operating System
+dist-check
+
