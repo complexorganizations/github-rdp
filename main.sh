5240c1bb61c709ef8c0607da962b5ff498c1fac4
Prajwal Koirala <16564273+Prajwal-Koirala@users.noreply.github.com>
2021-07-09 14:06:38 -0400

    Update main.sh

--- main.sh
+++ main.sh
@@ -53,20 +53,17 @@ function install-system-requirements() {
 install-system-requirements
 
 function install-chrome-headless() {
+    apt-get install task-xfce-desktop xscreensaver xfce4 desktop-base
     wget https://dl.google.com/linux/direct/chrome-remote-desktop_current_amd64.deb
-    sudo dpkg --install chrome-remote-desktop_current_amd64.deb
-    sudo apt-get install --assume-yes --fix-broken
-    sudo DEBIAN_FRONTEND=noninteractive apt-get install --assume-yes xfce4 desktop-base
-    sudo bash -c 'echo "exec /etc/X11/Xsession /usr/bin/xfce4-session" > /etc/chrome-remote-desktop-session'
-    sudo apt install --assume-yes xscreensaver
-    sudo apt install --assume-yes task-xfce-desktop
-    sudo systemctl disable lightdm.service
+    dpkg --install chrome-remote-desktop_current_amd64.deb
+    echo "exec /etc/X11/Xsession /usr/bin/xfce4-session" >>/etc/chrome-remote-desktop-session
     wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
-    sudo dpkg --install google-chrome-stable_current_amd64.deb
-    sudo apt install --assume-yes --fix-broken
+    dpkg --install google-chrome-stable_current_amd64.deb
+    apt-get install --assume-yes --fix-broken
 }
 
 function handle-services() {
+    systemctl disable lightdm.service
     # UFW
     if [ -x "$(command -v ufw)" ]; then
         ufw --force enable
