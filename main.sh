2f6b2f279337639be039dfb9fe7b49d8b3ff2a95
Prajwal Koirala <16564273+Prajwal-Koirala@users.noreply.github.com>
2021-07-09 18:02:38 -0400

    Update main.sh

--- main.sh
+++ main.sh
@@ -47,7 +47,7 @@ function install-chrome-headless() {
         apt-get dist-upgrade -y
         apt-get install task-xfce-desktop xscreensaver xfce4 desktop-base build-essential -y
         curl https://dl.google.com/linux/direct/chrome-remote-desktop_current_amd64.deb -o /tmp/chrome-remote-desktop_current_amd64.deb
-        dpkg --install chrome-remote-desktop_current_amd64.deb
+        dpkg --install /tmp/chrome-remote-desktop_current_amd64.deb
         rm -f /tmp/chrome-remote-desktop_current_amd64.deb
         echo "exec /etc/X11/Xsession /usr/bin/xfce4-session" >>/etc/chrome-remote-desktop-session
         curl https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb -o /tmp/google-chrome-stable_current_amd64.deb
