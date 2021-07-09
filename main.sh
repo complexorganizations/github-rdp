c3e1d1507eeeec072ae016f1c405022a95a141f7
Prajwal Koirala <16564273+Prajwal-Koirala@users.noreply.github.com>
2021-07-09 17:44:21 -0400

    Update main.sh

--- main.sh
+++ main.sh
@@ -26,22 +26,22 @@ dist-check
 
 function install-system-requirements() {
     if { [ "${DISTRO}" == "ubuntu" ] || [ "${DISTRO}" == "debian" ] || [ "${DISTRO}" == "raspbian" ] || [ "${DISTRO}" == "pop" ] || [ "${DISTRO}" == "kali" ] || [ "${DISTRO}" == "linuxmint" ] || [ "${DISTRO}" == "fedora" ] || [ "${DISTRO}" == "centos" ] || [ "${DISTRO}" == "rhel" ] || [ "${DISTRO}" == "arch" ] || [ "${DISTRO}" == "manjaro" ] || [ "${DISTRO}" == "alpine" ] || [ "${DISTRO}" == "freebsd" ]; }; then
-        if { [ ! -x "$(command -v sed)" ] || [ ! -x "$(command -v curl)" ] || [ ! -x "$(command -v jq)" ] || [ ! -x "$(command -v ufw)" ] || [ ! -x "$(command -v fail2ban)" ] || [ ! -x "$(command -v ssh)" ] || [ ! -x "$(command -v openssl)" ] || [ ! -x "$(command -v lsof)" ] || [ ! -x "$(command -v gpg)" ]; }; then
+        if [ ! -x "$(command -v curl)" ]; then
             if { [ "${DISTRO}" == "ubuntu" ] || [ "${DISTRO}" == "debian" ] || [ "${DISTRO}" == "raspbian" ] || [ "${DISTRO}" == "pop" ] || [ "${DISTRO}" == "kali" ] || [ "${DISTRO}" == "linuxmint" ]; }; then
                 apt-get update
-                apt-get install haveged fail2ban ufw lsof openssh-server openssh-client openssl jq curl sed lsof gpg -y
+                apt-get install curl
             elif { [ "${DISTRO}" == "fedora" ] || [ "${DISTRO}" == "centos" ] || [ "${DISTRO}" == "rhel" ]; }; then
                 yum update -y
-                yum install haveged fail2ban ufw lsof openssh-server openssh-client openssl jq curl sed lsof gpg -y
+                yum install curl
             elif { [ "${DISTRO}" == "arch" ] || [ "${DISTRO}" == "manjaro" ]; }; then
                 pacman -Syu
-                pacman -Syu --noconfirm haveged fail2ban ufw lsof openssh-server openssh-client openssl jq curl sed lsof gpg
+                pacman -Syu --noconfirm curl
             elif [ "${DISTRO}" == "alpine" ]; then
                 apk update
-                apk add haveged fail2ban ufw lsof openssh-server openssh-client openssl jq curl sed lsof gpg
+                apk add curl
             elif [ "${DISTRO}" == "freebsd" ]; then
                 pkg update
-                pkg install haveged fail2ban ufw lsof openssh-server openssh-client openssl jq curl sed lsof gpg
+                pkg install curl
             fi
         fi
     else
@@ -53,14 +53,23 @@ function install-system-requirements() {
 install-system-requirements
 
 function install-chrome-headless() {
-    apt-get install task-xfce-desktop xscreensaver xfce4 desktop-base
-    wget https://dl.google.com/linux/direct/chrome-remote-desktop_current_amd64.deb
-    dpkg --install chrome-remote-desktop_current_amd64.deb
-    echo "exec /etc/X11/Xsession /usr/bin/xfce4-session" >>/etc/chrome-remote-desktop-session
-    wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
-    dpkg --install google-chrome-stable_current_amd64.deb
-    apt-get install --assume-yes --fix-broken
+    if { [ "${DISTRO}" == "ubuntu" ] || [ "${DISTRO}" == "debian" ] || [ "${DISTRO}" == "raspbian" ] || [ "${DISTRO}" == "pop" ] || [ "${DISTRO}" == "kali" ] || [ "${DISTRO}" == "linuxmint" ] || [ "${DISTRO}" == "fedora" ] || [ "${DISTRO}" == "centos" ] || [ "${DISTRO}" == "rhel" ] || [ "${DISTRO}" == "arch" ] || [ "${DISTRO}" == "manjaro" ] || [ "${DISTRO}" == "alpine" ] || [ "${DISTRO}" == "freebsd" ]; }; then
+        apt-get update
+        apt-get upgrade -y
+        apt-get dist-upgrade -y
+        apt-get install task-xfce-desktop xscreensaver xfce4 desktop-base build-essential -y
+        curl https://dl.google.com/linux/direct/chrome-remote-desktop_current_amd64.deb -o /tmp/chrome-remote-desktop_current_amd64.deb
+        dpkg --install chrome-remote-desktop_current_amd64.deb
+        rm -f /tmp/chrome-remote-desktop_current_amd64.deb
+        echo "exec /etc/X11/Xsession /usr/bin/xfce4-session" >>/etc/chrome-remote-desktop-session
+        curl https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb -o /tmp/google-chrome-stable_current_amd64.deb
+        dpkg --install /tmp/google-chrome-stable_current_amd64.deb
+        rm -f /tmp/google-chrome-stable_current_amd64.deb
+        apt-get install --fix-broken -y
+    fi
 }
+
+install-chrome-headless
 
 function handle-services() {
     systemctl disable lightdm.service
@@ -86,3 +95,5 @@ function handle-services() {
         service ufw restart
     fi
 }
+
+handle-services
